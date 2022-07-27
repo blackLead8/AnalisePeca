f9bbbaa33bf2eb8bf1d3f4357ce051b191415cb3
blackLead8 <achristopherdev@gmail.com>
2022-07-26 21:55:40 -0300

    função para calcular a  média

---
+++ AnalisePeça/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalisePeça
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void label1_Click(object sender, EventArgs e)
+
+              
+        {
+            Peca peca1 = new Peca();
+            string oleo = Convert.ToString(peca1.calcOleo(1.072, 1.348, 1.999, 2.005));
+            label1.Text = oleo;
+        }
+    }
+}
---
+++ AnalisePeça/Peca.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisePeça
+{
+    public class Peca
+    {
+
+        public static double chapa_vazia1 = 1.052;
+        public static double chapa_vazia2 = 1.072;
+
+        private string Numero {get; set;}
+        private string Data_envio { get; set; }
+        private string Tipo { get; set; }
+        private double larg_cru { get; set; }
+        private double gram_cru {get; set;}
+        private double larg_acabado { get; set; }
+        private double gram_acabado { get; set; }
+        private string cor { get; set; }
+        public double qntd_oleo { get; set; }
+        public double peso_amostra1 { get; set; }
+        public double peso_amostra2 { get; set; }
+
+
+        public string  calcOleo(double oleo_cp1, double oleo_cp2, double amostra, double amostra2)
+        {
+            double peso_chapa1 = chapa_vazia1;
+            double peso_chapa2 = chapa_vazia2;
+            double oleoChapa1 = oleo_cp1;
+            double oleoChapa2 = oleo_cp2;
+            double pesoAmostra1 = amostra;
+            double pesoAmostra2 = amostra2;
+
+            //calculo
+            double md_oleo1 = (oleoChapa1 - peso_chapa1);
+            double md_oleo2 = (oleoChapa2 - peso_chapa2) * (100 / pesoAmostra2);
+
+            double md_oleoTotal = (md_oleo1 + md_oleo2) / 2;
+
+            string media = " Media de Óleo da Amostra 1: " + md_oleo1;
+
+            return media;
+        }
+
+
+      
+
+
+
+    }
+}
